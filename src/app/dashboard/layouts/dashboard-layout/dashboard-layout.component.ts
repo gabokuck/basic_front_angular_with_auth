faf43c49f508b7fb2031b33a6e2004901e0daaeb
Gabriel Rosa <gabokuck1990@gmail.com>
2024-06-24 16:14:53 -0600

    configuraciones iniciales

---
+++ src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-layout.component.html',
+  styleUrl: './dashboard-layout.component.css'
+})
+export class DashboardLayoutComponent {
+
+}
